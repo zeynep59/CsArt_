a4288bcc1c32aa79c140ba520c5dda06f625d2d7
Zeynep ÖZTÜRK <zynpztrk2002@gmail.com>
2022-12-28 12:09:01 +0300

    route pages
    route changes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,21 @@
 <?php
 
-echo "home page";
+$url = $_GET['url'] ?? 'home';
+$url = explode("/", $url);
+
+$page_name = trim($url[0]);
+$filename = "../app/pages/" . $page_name . ".php";
+
+if(file_exists(($filename))){
+    require_once $filename;
+}
+else{
+     require_once "../app/pages/404.php";
+}
+
+//echo "<pre>";
+//echo $filename;
+//print_r($url);
+//echo "home page";
 
 
