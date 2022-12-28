5d9e00f3bfae1f3013e11a30850033691212c769
Zeynep ÖZTÜRK <zynpztrk2002@gmail.com>
2022-12-28 19:07:37 +0300

    signup authentication
    signup authentication is completed. user can create account and these info were saved in database.

--- app/pages/login.php
+++ app/pages/login.php
@@ -1,8 +1,31 @@
 <?php
 
-if(!empty($_POST)){
-  
+if(!empty($_POST))
+{
+  //validate
+  $errors = [];
+
+  $query = "select * from users where email = :email limit 1";
+  $row = query($query, ['email'=>$_POST['email']]);
+
+  if($row)
+  {
+    $data = [];
+    if(password_verify($_POST['password'], $row[0]['password']))
+    {
+      //grant access
+      authenticate($row[0]);
+      redirect('admin');
+
+    }else{
+      $errors['email'] = "wrong email or password";
+    }
+
+  }else{
+    $errors['email'] = "wrong email or password";
+  }
 }
+?>
 
 ?>
 <!doctype html>
--- app/pages/signup.php
+++ app/pages/signup.php
@@ -18,6 +18,16 @@ if(!empty($_POST)){
 
     $errors['password'] = "Password must be 5 characters or more";
   }
+  else
+    if($_POST['password'] !== $_POST['retype_password'])
+    {
+      $errors['password'] = "Passwords do not match";
+    }
+
+    if(empty($_POST['terms']))
+    {
+      $errors['terms'] = "Please accept the terms";
+    }
   $query = "select id from users where email = :email limit 1";
     $email = query($query, ['email'=>$_POST['email']]);
 
@@ -41,7 +51,7 @@ if(!empty($_POST)){
     $data = [];
     $data['username'] = $_POST['username'];
     $data['email']    = $_POST['email'];
-    $data['role']     = $_POST['role'];
+    $data['role']     = "user";
     $data['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 
@@ -58,112 +68,136 @@ if(!empty($_POST)){
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <title>Login -CsArt</title>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <title>Login -CsArt</title>
 
 
-    
 
-    
 
-<link href="<?php echo ROOT?>/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
+
+  <link href="<?php echo ROOT?>/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
       }
+    }
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
+    .b-example-divider {
+      height: 3rem;
+      background-color: rgba(0, 0, 0, .1);
+      border: solid rgba(0, 0, 0, .15);
+      border-width: 1px 0;
+      box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+    }
 
-      .b-example-divider {
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
+    .b-example-vr {
+      flex-shrink: 0;
+      width: 1.5rem;
+      height: 100vh;
+    }
 
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
+    .bi {
+      vertical-align: -.125em;
+      fill: currentColor;
+    }
 
-      .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-      }
+    .nav-scroller {
+      position: relative;
+      z-index: 2;
+      height: 2.75rem;
+      overflow-y: hidden;
+    }
 
-      .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-      }
-
-      .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="<?php echo ROOT?>/assets/css/sign-in.css" rel="stylesheet">
-  </head>
-  <body class="text-center">
-    
-<main class="form-signin w-100 m-auto">
-  <form method="post">
-    <h1 class="h3 mb-3 fw-normal">Create Account</h1>
-
-    <div class="form-floating">
-      <input name="username" type="text" class="form-control mb-2" id="floatingInput" placeholder="Username">
-      <label for="floatingInput">Username</label>
-    </div>
-    <div class="form-floating">
-      <input name="email" type="email" class="form-control mb-2" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input name="password" type="password" class="form-control  mb-2" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
-    <div class="form-floating">
-      <input name="retype_password" type="password" class="form-control  mb-2" id="floatingPassword" placeholder="Retype Password">
-      <label for="floatingPassword">Password</label>
-    </div>
-
-    <div class="my-2">Already have an account? <a href="<?php echo ROOT?>/login"> Login here</a> </div>
-
-    <div class="checkbox mb-3">
-      <label>
-        <input name="terms" type="checkbox" value="remember-me"> Accept terms and conditions
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
-    <p class="mt-5 mb-3 text-muted">&copy; <?php echo date("M-Y")?></p>
-  </form>
-</main>
+    .nav-scroller .nav {
+      display: flex;
+      flex-wrap: nowrap;
+      padding-bottom: 1rem;
+      margin-top: -1px;
+      overflow-x: auto;
+      text-align: center;
+      white-space: nowrap;
+      -webkit-overflow-scrolling: touch;
+    }
+  </style>
 
 
-    
-  </body>
+  <!-- Custom styles for this template -->
+  <link href="<?php echo ROOT?>/assets/css/sign-in.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+
+  <main class="form-signin w-100 m-auto">
+    <form method="post">
+      <h1 class="h3 mb-3 fw-normal">Create Account</h1>
+
+      <?php if (!empty($errors)):?>
+      <div class="alert alert-danger">Please fix the errors below</div>
+    <?php endif;?>
+
+      <div class="form-floating">
+        <input value="<?=old_value('username')?>" name="username" type="text" class="form-control mb-2" id="floatingInput" placeholder="Username">
+        <label for="floatingInput">Username</label>
+      </div>
+      <?php if(!empty($errors['username'])):?>
+      <div class="text-danger"><?=$errors['username']?></div>
+      <?php endif;?>
+
+      <div class="form-floating">
+        <input  value="<?=old_value('email')?>" name="email" type="email" class="form-control mb-2" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <?php if(!empty($errors['email'])):?>
+      <div class="text-danger"><?=$errors['email']?></div>
+      <?php endif;?>
+
+      <div class="form-floating">
+        <input  value="<?=old_value('password')?>" name="password" type="password" class="form-control  mb-2" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+      <?php if(!empty($errors['password'])):?>
+      <div class="text-danger"><?=$errors['password']?></div>
+      <?php endif;?>
+
+      <div class="form-floating">
+        <input  value="<?=old_value('retype_password')?>" name="retype_password" type="password" class="form-control  mb-2" id="floatingPassword"
+          placeholder="Retype Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+
+      <div class="my-2">Already have an account? <a href="<?php echo ROOT?>/login"> Login here</a> </div>
+
+      <div class="checkbox mb-3">
+        <label>
+          <input  <?=old_checked('terms')?> name="terms" type="checkbox" value="remember-me"> Accept terms and conditions
+        </label>
+      </div>
+      <?php if(!empty($errors['terms'])):?>
+      <div class="text-danger"><?=$errors['terms']?></div>
+      <?php endif;?>
+      
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+      <p class="mt-5 mb-3 text-muted">&copy; <?php echo date("M-Y")?></p>
+    </form>
+  </main>
+
+
+
+</body>
+
 </html>
